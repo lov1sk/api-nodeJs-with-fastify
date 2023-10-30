569d02d182fe3abf360368f60393373305729dc0
Lucas Ribeiro Santana <lucassantana00@gmail.com>
2023-10-30 12:15:49 -0300

    fix: database config

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -4,7 +4,7 @@ import { knex as setupKnex, Knex } from "knex";
 import { env } from "../env";
 
 export const config: Knex.Config = {
-  client: "sqlite",
+  client: env.DATABASE_CLIENT === "sqlite" ? "sqlite" : "pg",
   connection:
     env.DATABASE_CLIENT === "sqlite"
       ? { filename: env.DATABASE_URL }
